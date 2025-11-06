012bf32223d8005b9ba8e9caa9faaa5830d870c0
mightypavan <pavanreddy339900@gmail.com>
2025-11-06 19:18:57 +0530

    extend token validity

--- Backend/routes/routers.js
+++ Backend/routes/routers.js
@@ -65,7 +65,7 @@ router.post("/api/auth/login", async (req, res)=>{
         const isMatch = await bcrypt.compare(password, user.password)
         if(!isMatch) return res.status(400).json({msg: "Invalid password"})
 
-        const token = jwt.sign({id: user._id, username: user.username, email: user.email}, JWT_SECRET, {expiresIn: "1h"})
+        const token = jwt.sign({id: user._id, username: user.username, email: user.email}, JWT_SECRET, {expiresIn: "7d"})
         res.json({
             token,
             user: {
