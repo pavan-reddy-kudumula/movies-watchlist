ea9ac278cfbe8da4306ba6c6ad4af19fb0ab31c1
mightypavan <pavanreddy339900@gmail.com>
2026-01-25 15:13:00 +0530

    set JWT and cookie to 1-year lifetime

--- Backend/lib/utils.js
+++ Backend/lib/utils.js
@@ -1,9 +1,9 @@
 import jwt from "jsonwebtoken"
 
 export const generateToken = (userId, res) => {
-    const token = jwt.sign({userId}, process.env.JWT_SECRET, {expiresIn:"7d"})
+    const token = jwt.sign({userId}, process.env.JWT_SECRET, { expiresIn: "365d" })
     res.cookie("jwt", token, {
-        maxAge: 7 * 24 * 60 * 60 * 1000,
+        maxAge: 365 * 24 * 60 * 60 * 1000,
         httpOnly: true,
         sameSite: process.env.NODE_ENV === "development" ? "lax" : "none",
         secure: process.env.NODE_ENV !== "development"
